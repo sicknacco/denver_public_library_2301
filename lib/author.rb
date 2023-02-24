1899037c17008ec45c4ffd865156faf2cf51d319
Nick Sacco <nicksacco10@gmail.com>
2023-02-24 08:56:45 -0800

    Feat: write method

--- lib/author.rb
+++ lib/author.rb
@@ -10,4 +10,10 @@ class Author
   def name
     "#{author_info[:first_name]} #{author_info[:last_name]}"
   end
+
+  def write(title, publication_date)
+    new_book = Book.new({author_first_name: "#{author_info[:first_name]}", author_last_name: "#{author_info[:last_name]}", title: title, publication_date: publication_date})
+    @books << new_book
+    new_book
+ end
 end
