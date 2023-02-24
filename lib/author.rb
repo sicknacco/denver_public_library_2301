1b9fd591b21e349af3c0d6815082060c02793197
Nick Sacco <nicksacco10@gmail.com>
2023-02-24 08:36:00 -0800

    Feat: author name method

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,11 @@
 class Author
-  def initialize(author_hash)
+  attr_reader :author_info
 
+  def initialize(author_hash)
+    @author_info = author_hash
+  end
+
+  def name
+    "#{author_info[:first_name]} #{author_info[:last_name]}"
   end
 end
