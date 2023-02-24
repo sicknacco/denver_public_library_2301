0b425a64973b6683dc2d73612de10ad5e3e62ee5
Nick Sacco <nicksacco10@gmail.com>
2023-02-24 09:20:04 -0800

    Test: start with no books or authors

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -15,4 +15,12 @@ RSpec.describe Library do
   it 'has a name' do
     expect(@dpl.name).to eq("Denver Public Library")
   end
+
+  it 'starts with no books' do
+    expect(@dpl.books).to eq([])
+  end
+
+  xt 'starts with no authors' do
+    expect(@dpl.authors).to eq([])
+  end
 end
