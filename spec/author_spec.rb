b804106d0c848d9f5eafb1cb2fdff9e4cdb99113
Nick Sacco <nicksacco10@gmail.com>
2023-02-24 08:56:13 -0800

    Test: refactor write tests

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -5,22 +5,23 @@ require './lib/author'
 RSpec.describe Author do
   before(:each) do
     @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-    @jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
   end
-
+  
   it 'exists' do
     expect(@charlotte_bronte).to be_an_instance_of(Author)
   end
-
+  
   it 'author has a name' do
     expect(@charlotte_bronte.name).to eq("Charlotte Bronte")
   end
-
+  
   it 'starts with no books' do
     expect(@charlotte_bronte.books).to eq([])
   end
-
+  
   it 'can write books' do
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
     expect(@jane_eyre.class).to eq(Book)
     expect(@jane_eyre.title).to eq("Jane Eyre")
   end
